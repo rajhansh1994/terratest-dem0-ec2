f6eae87da16adc420be78c7ce6ba296ee64aa325
Rajhansh <kumar.rajhansh@capgemini.com>
2021-10-27 16:26:00 +0530

    marrying tf with ansible

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "example" {
   ami                    = "ami-0747bdcabd34c712a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name = "anz_demo"
 
   # When the instance boots, start a web server on port 8080 that responds with "Hello, World!".
   user_data = <<EOF
@@ -10,6 +11,11 @@ resource "aws_instance" "example" {
 echo "Hello, World!" > index.html
 nohup busybox httpd -f -p 8080 &
 EOF
+
+  provisioner "local-exec" {
+
+    command = "sleep 60s;chmod 600 anz_demo.pem;ANSIBLE_HOST_KEY_CHECKING False ansible-playbook -u ec2-user -i '${aws_instance.example.public_ip},' --private-key anz_demo.pem playbook.yml"
+  }
 }
 
 # Allow the instance to receive requests on port 8080.
